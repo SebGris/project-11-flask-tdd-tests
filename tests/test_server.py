7d9f5f43e9d13c3c62064bd1c0abec561e0843ed
Sébastien Grison <74866413+SebGris@users.noreply.github.com>
2025-08-12 17:28:27 +0200

    Ajout de tests avec Pytest pour le serveur et configuration des fixtures

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,6 +1,21 @@
+import pytest
+from server import app as flask_app
+
 # def func(x):
 #     return x + 1
 
 
 # def test_answer():
 #     assert func(3) == 4
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+def test_request_example(client):
+    response = client.get("/")
+    assert b"Welcome to the GUDLFT Registration Portal!" in response.data
